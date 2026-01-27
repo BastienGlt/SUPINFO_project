de3fa404c8e1f4a5ed82906a172db962ea92416d
Bastien <guillemet.bastien59@gmail.com>
2026-01-27 14:29:02 +0100

    patch

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2/promise');
 
 // Connexion à MySQL (Pool de connexions)
 const pool = mysql.createPool({
-  host: '193.38.250.100',
-  user: 'main',
-  password: '28c!5q6Xu',
-  database: 'supinfo',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
